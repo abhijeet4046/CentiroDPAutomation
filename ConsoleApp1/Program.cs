dfb39d0474afd821718a0720f1aab486e958b398
Abhijeet patil <abhijeet4046@gmail.com>
2020-04-23 11:15:43 +0530

    added projects with solution

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DeliveryPerformanceAutomation dpAutomation=new DeliveryPerformanceAutomation();
+            dpAutomation.LoginDelivereyPerformance();
+        }
+    }
+}
